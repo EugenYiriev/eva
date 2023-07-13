564ae93237bd8a66c2eb80d7d9e2a11b0035c342
zhenya.urev <zhenyaurev@gmail.com>
2023-07-13 14:09:34 +0300

    fix file

--- src/Componetns/VideoReviews/VideoReviews.tsx
+++ src/Componetns/VideoReviews/VideoReviews.tsx
@@ -1,53 +1,49 @@
-'use client'
-import React, {useEffect, useState} from 'react'
-import client from '../../../sanity/lib/client'
-import imageUrlBuilder from '@sanity/image-url'
-import {Subtitle} from '../Tags/SubTitle/SubTitle'
-import styles from './style.module.scss'
-import {VideoPlayer} from '../VideoPlayer/VideoPlayer'
+"use client";
+import React, { useEffect, useState } from 'react';
+import client from '../../../sanity/lib/client';
+import imageUrlBuilder from '@sanity/image-url';
+import { Subtitle } from '../Tags/SubTitle/SubTitle';
+import styles from './style.module.scss';
+import { VideoPlayer } from '../VideoPlayer/VideoPlayer';
 
 interface Video {
-    videoLink: string
-    backgroundImage: string
+  videoLink: string;
+  backgroundImage: string;
 }
 
 interface VideoReviewsProps {}
 
 export const VideoReviews: React.FC<VideoReviewsProps> = () => {
-  const [videos, setVideos] = useState<Video[]>([]);
-  const [title, setTitle] = useState<string>('');
+  const [videoReviews, setVideoReviews] = useState<{ title: string; videos: Video[] }>({
+    title: '',
+    videos: [],
+  });
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await client.fetch('*[_type == "reviewsVideo"]');
-        setVideos(response[0].videos);
-        setTitle(response[0].title);
+        setVideoReviews({ title: response[0].title, videos: response[0].videos });
       } catch (error) {
         console.error('Error:', error);
       }
     };
 
-        fetchData()
-    }, [])
+    fetchData();
+  }, []);
 
-    const builder = imageUrlBuilder(client)
+  const builder = imageUrlBuilder(client);
 
   return (
     <div className={styles.videoReviews}>
-      <Subtitle text={title} />
+      <Subtitle text={videoReviews.title} />
       <div className={styles.videoContainer}>
-        {videos.map((video, index) => {
+        {videoReviews.videos.map((video, index) => {
           const imageUrl = video.backgroundImage ? builder.image(video.backgroundImage).url() : '';
 
           return (
             <div key={index} className={styles.videoItem}>
-              <VideoPlayer
-                videoId={video.videoLink}
-                posterImageUrl={imageUrl}
-                width={606}
-                height={332}
-              />
+              <VideoPlayer videoId={video.videoLink} posterImageUrl={imageUrl} width={606} height={332} />
             </div>
           );
         })}
